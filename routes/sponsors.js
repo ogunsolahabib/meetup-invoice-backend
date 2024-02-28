570e6d99a17076bef00c92af5604591365002393
Habib Ogunsola <ogunsolahabib@yahoo.com>
2024-02-28 01:35:47 +0000

    sponsors CRUD endpoints

--- routes/sponsors.js
+++ routes/sponsors.js
@@ -5,21 +5,29 @@ const getPutFields = require('../utils/getPutFields');
 const router = Router();
 
 router.get('/', (request, response) => {
-    pool.query('SELECT * FROM sponsors', (err, res) => {
-        if (err) return response.send(err);
-        response.send(res.rows);
-    });
+    try {
+        pool.query('SELECT * FROM sponsors', (err, data) => {
+            if (err) return response.send(err);
+            response.send(data.rows);
+        });
+    } catch (err) {
+        response.send(err);
+    }
 });
 
 router.post('/', (request, response) => {
-    const { sponsor_id, total_amount } = request.body;
+    const { name, street, city, phone } = request.body;
+    try {
 
-    pool.query('INSERT INTO sponsors(sponsor_id, invoice_date, total_amount) VALUES ($1, current_timestamp, $2)',
-        [sponsor_id, total_amount],
-        (err, res) => {
-            if (err) return response.send(err);
-            response.redirect('/sponsors');
-        });
+        pool.query('INSERT INTO sponsors(name, street, city, phone) VALUES ($1, $2, $3, $4)',
+            [name, street, city, phone],
+            (err, data) => {
+                if (err) return response.send(err);
+                response.send('success');
+            });
+    } catch (err) {
+        response.send(err);
+    }
 });
 
 router.put('/:id', (request, response) => {
@@ -28,11 +36,10 @@ router.put('/:id', (request, response) => {
     const { setClause, values } = getPutFields(request.body);
 
     try {
-        pool.query(`UPDATE sponsors SET ${setClause} WHERE invoice_id = $2`, [...values, +id], (err, res) => {
+        pool.query(`UPDATE sponsors SET ${setClause} WHERE invoice_id = $2`, [...values, +id], (err, data) => {
             if (err) return response.send(err);
-
-            if (res) {
-                response.send(res.rows)
+            if (data) {
+                response.send(data.rows)
             }
         });
 
@@ -41,6 +48,15 @@ router.put('/:id', (request, response) => {
     }
 });
 
-
+router.delete('/:id', (request, response) => {
+    const { id } = request.params;
+    try {
+        pool.query('DELETE FROM sponsors WHERE id=$1', [+id], (err, data) => {
+            response.send(data);
+        })
+    } catch (err) {
+        response.send(err);
+    }
+});
 
 module.exports = router;
