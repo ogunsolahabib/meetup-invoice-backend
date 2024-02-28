5bbf5a30b487cc55f81c12b290d3d67937f1c617
Habib Ogunsola <ogunsolahabib@yahoo.com>
2024-02-28 00:50:29 +0000

    util for updating table

---
+++ utils/getPutFields.js
@@ -0,0 +1,14 @@
+const getPutFields = (body) => {
+
+    const fields = Object.keys(body).filter(Boolean);
+
+    const setClause = fields
+        .map((field, index) => `${field}=$${index + 1}`)
+        .join(', ');
+
+    const values = Object.values(body);
+
+    return { setClause, values }
+}
+
+module.exports = getPutFields;
