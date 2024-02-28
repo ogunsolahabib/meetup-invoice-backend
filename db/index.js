064058404099a5472e1525486ec820ac97bdbf26
Habib Ogunsola <ogunsolahabib@yahoo.com>
2024-02-28 18:24:16 +0000

    fix db url

--- db/index.js
+++ db/index.js
@@ -8,7 +8,7 @@ const user = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
 const port = process.env.PORT;
 
-const dbUrl = `postgresql://${user}:${password}@${host}:${port}/${database}`
+const dbUrl = `postgresql://${user}:${password}@${host}:${port}/${database}${host === "localhost" ? "" : '?sslmode=verify-full'}`
 
 const client = new Client(dbUrl);
 
