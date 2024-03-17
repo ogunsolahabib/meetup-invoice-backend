0fd0cdc08fe9a50df9ea534851c81c4755aba194
Habib Ogunsola <ogunsolahabib@yahoo.com>
2024-03-17 23:06:47 +0000

    setup CORS middleware

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const express = require("express");
 const routes = require("./routes");
 const bodyParser = require("body-parser");
 
+const cors = require("cors");
+
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
