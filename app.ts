164958c73eca46057057fbbc42d85f6295ed219c
Habib Ogunsola <ogunsolahabib@yahoo.com>
2024-04-15 02:15:55 +0100

    replace node with bun. setup typescript

---
+++ app.ts
@@ -0,0 +1,24 @@
+
+import express from "express";
+import routes from "./routes";
+import cors from "cors";
+import authMiddleware from "./middlewares/authMiddleware";
+
+const app = express();
+
+app.use(cors());
+
+app.use(authMiddleware);
+
+app.use(express.json());
+
+app.use(routes);
+
+app.get('/', (_, res) => { res.send('success') })
+
+
+const port = 4000;
+
+app.listen(port, () => console.log("listening on port " + port));
+
+export default app;
